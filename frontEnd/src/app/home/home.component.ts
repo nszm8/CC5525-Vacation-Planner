8abee6423fefd3701abf2c1e2a345c02b6af8a7e
nszm8 <nszm8@umsystem.edu>
2023-05-05 23:49:36 -0500

    Integrated the rapid api with front end

--- frontEnd/src/app/home/home.component.ts
+++ frontEnd/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  numberOfDays: number = 0;
+  destination: string = '';
+
+  constructor(private router: Router,private http: HttpClient) {}
+
+  vacationDetails()
+    {
+      let headers = new HttpHeaders({
+        'x-rapidapi-host': 'ai-trip-planner.p.rapidapi.com',
+        'x-rapidapi-key': 'fb4362bc11mshd9c08352a7e2038p1b7338jsn1434173b1a3d'
+      });
+
+      const params = new HttpParams().set
+                  ('days', this.numberOfDays).set('destination', this.destination);
+
+      this.http.get('https://ai-trip-planner.p.rapidapi.com/', {
+				headers: headers, params: params
+			})
+			.subscribe(data => {
+				console.log(data);
+			});
+    }
 }
--- frontEnd/src/app/login/login.component.ts
+++ frontEnd/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
